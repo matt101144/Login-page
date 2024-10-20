432434ab628bd48f48abfadc46ba2388858c7e01
matt101144 <matt.neville10@outlook.com>
2024-10-20 17:01:58 +0100

    added in the javascript for preventing refresh, validating email and password and writing a succes or error message depending on result

---
+++ index.js
@@ -0,0 +1,23 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    
+  
+    const emailInput = document.querySelector(".email").value; 
+    const passwordInput = document.querySelector(".password").value; 
+    
+    
+    const correctEmail = "username@login.com";
+    const correctPassword = "password123";
+    
+    const message = document.querySelector("#message");
+
+    // Validate email and password
+    if (emailInput === correctEmail && passwordInput === correctPassword) {
+        message.style.color = "green";
+        message.textContent = "Login successful!"; 
+        
+    } else {
+        message.style.color = "red";
+        message.textContent = "Incorrect email or password."; 
+    }
+});
